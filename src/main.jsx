af608718fed6e073dca690696226a5ee2a60f1dd
Marvelous rose <102363036+rhose1234@users.noreply.github.com>
2023-09-17 14:35:41 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import {BrowserRouter} from 'react-router-dom'
+
+const root = ReactDOM.createRoot(
+document.getelementById("root")
+);
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+      </BrowserRouter>
   </React.StrictMode>,
-)
+);
