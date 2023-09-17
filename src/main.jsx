30f9b4ca4668828c7161dc865453fa7a8ef20613
Marvelous rose <102363036+rhose1234@users.noreply.github.com>
2023-09-17 22:36:16 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,13 @@ import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { BrowserRouter } from 'react-router-dom';
 
-const root = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(
+const renderApp = () => (
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  root
+    <App />
+  </React.StrictMode>
 );
+
+ReactDOM.render(renderApp(), rootElement);
